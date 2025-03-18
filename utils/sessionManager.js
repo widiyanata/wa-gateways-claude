94743839a32c3afa2e438753f6d41095f8fa1af4
widiyanata <widyanata48@gmail.com>
2025-03-19 00:22:07 +0700

    folder sessions utk vercel

--- utils/sessionManager.js
+++ utils/sessionManager.js
@@ -10,7 +10,8 @@ const pino = require("pino");
 const { createAIManager } = require("./aiManager");
 
 const logger = pino({ level: "silent" });
-const SESSIONS_DIR = path.join(process.cwd(), "sessions");
+// const SESSIONS_DIR = path.join(process.cwd(), "sessions");
+const SESSIONS_DIR = path.join('/tmp', 'sessions');
 
 exports.createSessionManager = (io) => {
   const sessions = new Map();
